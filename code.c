de64df177e45e401ddb2ddb5ae31367a396fc22c
theo <theodorotheodore@gmail.com>
2021-05-18 06:51:10 -0300

    Now our program already can get the size of the Fibonacci's sequence

--- code.c
+++ code.c
@@ -2,6 +2,21 @@
 #include <stdio.h>
 
 // the main function has been declared
-int main(){
+int main()
+{
+    // here we ask for the user which is the size of our sequence
+    printf("\nPlease insert here the size that you want to our sequence: ");
 
+    // setting the variable that we'll store the size of our sequence and in the line bellow we scan and get this value
+    int size;
+    scanf("%i", &size);
+
+    // print an null line
+    printf("\n");
+
+    // printing for check if exists some bugs
+    printf("%i", size);
+
+    // zero-returning code
+    return 0;
 }
