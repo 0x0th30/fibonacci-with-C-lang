c52cc1295a3819b321b3e22671ac12b3c79f8258
theo <theodorotheodore@gmail.com>
2021-05-27 12:19:19 -0300

    Everthing finished!

--- code.c
+++ code.c
@@ -10,13 +10,26 @@ int main()
     // setting the variable that we'll store the size of our sequence and in the line bellow we scan and get this value
     int size;
     scanf("%i", &size);
-
-    // print an null line
-    printf("\n");
+    printf("");
 
     // printing for check if exists some bugs
     printf("%i", size);
+    printf("\n");
+
+    // in the 3 lines bellow we declare our variables that will be used in the operation
+    int first = 1;
+    int secnd = 1;
+    int sum;
+
+    // this iteration will create an cicle that calculates the fibonacci's sequence
+    for(int i = 0; i < size; i++){
+        sum = first + secnd;
+        secnd = first;
+        first = sum;
+        printf("%i - ", sum);
+    }
 
     // zero-returning code
     return 0;
 }
+
